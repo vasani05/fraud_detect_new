54ed7ba3ac4bf097efb0757c456ad61a15bd3190
vasani05 <akash.vmc13@sot.pdpu.ac.in>
2023-07-29 17:04:15 +0530

    added new models to the file

--- model.py
+++ model.py
@@ -1,1 +1,4 @@
 print("this is the model file that i have created and this is the file that i would be using to create different ml model")
+
+def model1():
+    print("This is the new model that we have added to the file")
