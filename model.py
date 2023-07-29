60edd733d2e85e0c3cd07ab0197b43dfef249f6c
vasani05 <akash.vmc13@sot.pdpu.ac.in>
2023-07-29 17:13:46 +0530

    This is the final model

--- model.py
+++ model.py
@@ -3,5 +3,4 @@ print("this is the model file that i have created and this is the file that i wo
 def model1():
     print("This is the new model that we have added to the file")
 
-def model2():
-    print("Testing this model")
+print("The final model is model1 since it has better accuracy and performance")
